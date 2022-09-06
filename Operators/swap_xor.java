ff0b02b18e17e674355c8860eaab2507bcc23ef7
manish <43578519+manish8621@users.noreply.github.com>
2022-09-06 22:32:58 +0530

    2 added total 3

---
+++ Operators/swap_xor.java
@@ -0,0 +1,18 @@
+public class swap_xor {
+    public static void main(String[] args) {
+        int a=3,b=4,c=0;
+        // for (int i = 0; i <= 10; i++) {
+        // c = a^i;
+ 
+        // System.out.println("a:"+Integer.toBinaryString(a)+" is "+a+" b:"+Integer.toBinaryString(i)+" is "+i+" c "+Integer.toBinaryString(c)+" is "+c);
+        
+        System.out.println(Integer.toBinaryString(a)+" "+Integer.toBinaryString(b)+" "+Integer.toBinaryString(a^b));
+        // System.out.println(Integer.toBinaryString(a));
+        // System.out.println(Integer.toBinaryString(b));
+        // System.out.println(Integer.toBinaryString((a = a^b)));
+        // System.out.println(Integer.toBinaryString((b = a^b)));
+        // System.out.println(Integer.toBinaryString((a = a^b)));
+
+    }
+
+}
---
+++ Operators/unique_element.java
@@ -0,0 +1,9 @@
+public class unique_element {
+    public static void main(String[] args) {
+        int[] a ={1,2,9,6,7,6,4,1,4,2,9}; 
+        int c=0;
+        for (int item : a) 
+            c ^= item; //every pair
+        System.out.println(c);
+    }
+}
