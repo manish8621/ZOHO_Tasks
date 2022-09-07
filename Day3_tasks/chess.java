0d10c8db8591b5138a71c080e59bcb1abd11413c
manish <43578519+manish8621@users.noreply.github.com>
2022-09-07 23:07:25 +0530

    Update chess.java

--- Day3_tasks/chess.java
+++ Day3_tasks/chess.java
@@ -1,22 +1,11 @@
 import java.math.BigInteger;
-import java.util.ArrayList;
 
 public class chess {
     public static void main(String[] args) {
-        BigInteger start = new BigInteger("1");
+        BigInteger start = new BigInteger("1"),bi = new BigInteger("0");
         int end =63;
-        BigInteger bi = new BigInteger("0");
         for (int i = 0; i <= end; i++) 
             bi = bi.add(start.multiply((new BigInteger("2")).pow(i)));
         System.out.println(bi); //18446744073709551615
-
-        ArrayList<Integer> a = new ArrayList<>();
-        method(a);
-        System.out.println(a);
-
-    }
-    static void  method(ArrayList<Integer> l)
-    {
-        l.add(1);
     }
 }
