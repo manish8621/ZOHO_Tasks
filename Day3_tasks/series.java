2fc3164aa5c8b4705c2445b2554b14134ea6d510
manish <43578519+manish8621@users.noreply.github.com>
2022-09-07 23:11:29 +0530

    Update series.java

--- Day3_tasks/series.java
+++ Day3_tasks/series.java
@@ -2,8 +2,11 @@ import java.math.BigInteger;
 
 public class series {
     public static void main(String[] args) {
-        BigInteger start = new BigInteger("5");
-        int end = 100;
-        System.out.println((start.multiply((new BigInteger("2")).pow(end)))); //40
+        Scanner scan = new Scanner(System.in);
+        System.out.print("start:");
+        BigInteger start = new BigInteger(Sting.valueOf(scan.nextInt()));
+        System.out.print("end:");
+        int end = scan.nextInt();
+        System.out.println((start.multiply((new BigInteger("2")).pow(end)))); // start ->5  end->3  ans->40
     }
 }
