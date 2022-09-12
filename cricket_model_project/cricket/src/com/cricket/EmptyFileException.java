db39ab6083e1fc7317d92558a09d0737a9eca7fe
manish <43578519+manish8621@users.noreply.github.com>
2022-09-12 13:13:59 +0530

    Delete cricket_model_project/cricket directory

--- cricket_model_project/cricket/src/com/cricket/EmptyFileException.java
+++
@@ -1,8 +0,0 @@
-package com.cricket;
-
-public class EmptyFileException extends Exception{
-    EmptyFileException(String path)
-    {
-        super("The file("+path+") you trying to read is empty");
-    }
-}
--- cricket_model_project/cricket/src/com/cricket/NotEnoughTeams.java
+++
@@ -1,8 +0,0 @@
-package com.cricket;
-
-public class NotEnoughTeams extends Exception {
-    NotEnoughTeams()
-    {
-        super("Not enough teams in Db .");
-    }
-}
